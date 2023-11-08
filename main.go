62acfa75c4d9fbbbccedb82db479ddb1be924055
Patrick Baciu <patrick.baciu@hotmail.com>
2023-11-08 13:01:42 -0500

    Add lookup registry

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import (
   "net/http"
   "fmt"
   "net"
+  "slices"
 )
 
+var registry = make(map[uint16][]string)
+
 func main()  {
+  registerHost(5001, "https://google.com")
+  registerHost(5001, "https://cnn.com")
+  
   go http.ListenAndServe(":5001", http.HandlerFunc(fiveThousandHandler))
   http.ListenAndServe(":5002", http.HandlerFunc(fiveThousandHandler))
 }
@@ -26,7 +32,7 @@ func sevenThousandHandler() {
 
 func identifyPort(req *http.Request) uint16 {
   context := req.Context()
-  
+ 
   addr, ok := context.Value(http.LocalAddrContextKey).(net.Addr)
   if !ok {
     fmt.Println("ERROR: request address not found.")
@@ -35,4 +41,11 @@ func identifyPort(req *http.Request) uint16 {
   port := addr.(*net.TCPAddr).Port
  
   return uint16(port)
+}
+
+// Registers host as a potential target for incoming request from port.
+func registerHost(port uint16, host string) {
+  if !slices.Contains(registry[port], host) {
+    registry[port] = append(registry[port], host)
+  }
 }
